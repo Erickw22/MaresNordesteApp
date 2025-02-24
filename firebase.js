304c4cf677c5ff89e5d4b0338eaf7697a586a21d
Erickw22 <160823597+Erickw22@users.noreply.github.com>
2025-02-24 20:36:44 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-// Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAocbQjdGSkrM02VZgNOZxeELMnosabmsM",
-  authDomain: "mapp-fn.firebaseapp.com",
-  projectId: "mapp-fn",
-  storageBucket: "mapp-fn.appspot.com",
-  messagingSenderId: "933586749255",
-  appId: "1:933586749255:web:35d9c0c609a3e950872e62"
+  //...
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-// Inicialização do Firebase Auth com persistência
-let authentication;
-if (!authentication) {
-  authentication = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-}
-
-export { db, authentication };
